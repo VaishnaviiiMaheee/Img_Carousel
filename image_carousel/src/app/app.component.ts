136a177ad6ef138ec8731f45fc35d26205fafb81
Vaishnavi Maheshwari <vmahe.03.08.01@gmail.com>
2023-10-16 09:55:32 +0530

    My image carousel

---
+++ image_carousel/src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'image_carousel';
+  images:string[] = [
+    "https://fastly.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U",
+    "https://fastly.picsum.photos/id/866/200/300.jpg?hmac=rcadCENKh4rD6MAp6V_ma-AyWv641M4iiOpe1RyFHeI",
+    "https://fastly.picsum.photos/id/1/200/300.jpg?hmac=jH5bDkLr6Tgy3oAg5khKCHeunZMHq0ehBZr6vGifPLY",
+    "https://fastly.picsum.photos/id/10/2500/1667.jpg?hmac=J04WWC_ebchx3WwzbM-Z4_KC_LeLBWr5LZMaAkWkF68",
+    "https://fastly.picsum.photos/id/24/4855/1803.jpg?hmac=ICVhP1pUXDLXaTkgwDJinSUS59UWalMxf4SOIWb9Ui4"
+  ];
+  currentImageIndex:number=0;
+  get currentImage(): string{
+    return this.images[this.currentImageIndex];
+  }
+  nextImage(){
+  this.currentImageIndex=(this.currentImageIndex+1)%this.images.length;
+  }
+  prevImage(){
+    this.currentImageIndex=(this.currentImageIndex-1+this.images.length)%this.images.length;
+  }
+}
+
+
